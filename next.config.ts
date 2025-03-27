1cad18ff314f11c1a863ed3ead7eb70af378f7e2
ceednee <ceednee@gmail.com>
2025-03-27 01:35:53 +0100

    [wip] Comments options for next.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // devIndicators: false,
+  // images: {
+  //   unoptimized: true,
+  // },
+  // reactStrictMode: true,
+  // swcMinify: true,
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // output: "export",
+  // distDir: "build",
+  // trailingSlash: true,
 };
 
 export default nextConfig;
